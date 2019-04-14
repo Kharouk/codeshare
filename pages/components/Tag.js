08f4432d0900a24d4511b65f833437369d1418ff
Alex Kharouk <akharoukpp@gmail.com>
2019-04-14 19:04:59 +0100

    formatting check

--- pages/components/Tag.js
+++ pages/components/Tag.js
@@ -1,10 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 import Link from "next/link"
 
 const Button = styled.div`
-  /* This renders the buttons above... Edit me! */
   display: inline-block;
   border-radius: 3px;
   padding: 0.5rem 0;
