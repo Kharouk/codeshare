98a469acf1755cb5503bcb26cb60cf42ad4776e9
Alex Kharouk <akharoukpp@gmail.com>
2019-04-14 14:28:01 +0100

    reconfigure files and refresh packages

--- pages/Index.js
+++ pages/Index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React from "react"
+import Link from "next/link"
 
 const Index = () => {
   return (
     <div>
       <h2>Welcome to CodeShare</h2>
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
+      <Link href="/login">
+        <a>Login</a>
+      </Link>
     </div>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
