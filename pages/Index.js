f9b35ccafa8610fd052b3e762e73b22c6969d8bc
Alex Kharouk <akharoukpp@gmail.com>
2019-04-14 17:17:09 +0100

    Update styled to work with babel and SSR

--- pages/Index.js
+++ pages/Index.js
@@ -1,13 +1,12 @@
 import React from "react"
 import Link from "next/link"
+import Tag from "./components/Tag"
 
 const Index = () => {
   return (
     <div>
       <h2>Welcome to CodeShare</h2>
-      <Link href="/about" title="About Page">
-        <a>About Page</a>
-      </Link>
+      <Tag href="/about">About us</Tag>
       <Link href="/login">
         <a>Login</a>
       </Link>
--- pages/components/Tag.js
+++ pages/components/Tag.js
@@ -1,12 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled, { css } from "styled-components"
+import Link from "next/link"
 
-const Tag = ({ children }) => {
-  return <a>{children}</a>
+const Button = styled.div`
+  /* This renders the buttons above... Edit me! */
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+`
+
+const Tag = ({ children, href }) => {
+  return (
+    <Button>
+      <Link href={href}>
+        <a>{children}</a>
+      </Link>
+    </Button>
+  )
 }
 
 Tag.propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired
 }
 
 export default Tag
