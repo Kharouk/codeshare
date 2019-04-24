00cfb5d702145ca686e9a0497c0aadf5f925c217
Alex Kharouk <akharoukpp@gmail.com>
2019-04-24 19:18:19 +0100

    create local db file

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,15 @@
 // eslint-disable-next-line no-undef
 const { Prisma } = require(`prisma-binding`)
+
+// this file connects to the remote prisma DB  and gives us the ability to query it with JS
+
+const db = new Prisma({
+  typeDefs: `generated/prisma-client/prisma-scheme.js`,
+  // eslint-disable-next-line no-undef
+  endpoint: process.env.LOCALHOST_URL,
+  // eslint-disable-next-line no-undef
+  secret: process.env.PRISMA_SECRET,
+  debug: true
+})
+
+module.exports = db
